4c08b8fa875aa36fb26a888d3acfa8dd8bba90ed
mayragsss <mayra.g.dias@hotmail.com>
2025-07-23 21:52:10 +0100

    'add'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products',
+  imports: [],
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent {
+
+}
