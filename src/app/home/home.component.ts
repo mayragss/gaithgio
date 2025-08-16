01a4b8f2c85f29cdf6d5322cf63ae23163106fba
mayragsss <mayra.g.dias@hotmail.com>
2025-08-16 12:46:07 +0100

    add products page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { NavbarComponent } from '../components/navbar/navbar.component';
 
 @Component({
   selector: 'home',
-  imports: [ButtonModule],
+  imports: [ButtonModule, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../components/navbar/navbar.component';
 
 @Component({
   selector: 'products',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
